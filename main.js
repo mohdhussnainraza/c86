52e3bb8398d4e39dfd89c7414e9d7b393ecaf12c
mohdhussnainraza <92803788+mohdhussnainraza@users.noreply.github.com>
2022-05-19 10:00:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,22 @@
-var las_position_of_x, last_position_of_y;
-color ="blank";
-width_of_line = 2;
+var canvas = new fabric.Canvas('mycanvas');
+ var x= document.getElementById("myAudio");
 
-canvas = document.getElementById('myCanvas');
-ctx = canvas.getContext("2d");
+function new_image()
+{
+	fabric.Image.fromURL('BirthdayImage.jpg', function(Img){
+        block_image_object =Img;
 
-var width = screen.width;
-new_width = screen.width - 70;
-new_height = screen.height - 300;
-    if(width < 992)
-    {
-    document.getElementById("myCanvas").width = new_width;
-    document.getElementById("myCanvas").height = new_height;
-    document.body.style.overflow = "hidden";
-    }
+        block_image_object.scaleToWidth(700);
+        block_image_object.scaleToHeight(510);
+        block_image_object.set({
+            top:0,
+            left:0
+        });
+        canvas.add(block_image_object);
+    });
+	
+}
 
-    canvas.addEventlistener("touchstart", my_touchstart);
-
-    function my_touchstart(e)
-    {
-        console.log("my_touchstart");
-
-      color = document.getElementById("color").value;
-      width_of_line = document.getElementById("width_of_line").value;
-
-
-      last_position_of_x = e.touches[0].clientX - canvas.offsetLeft;
-      last_position_of_y = e.touches[0].clientY - canvas.offsetTop;
-    }
-    canvas.addEventListener("touchmove", my_touchmove);
-    
-
-    function my_touchmove(e)
-    {
-
-        console.log("my_touchMove");
-        current_position_of_touch_x = e.touches[0].clientY - canvas.offsetLeft;
-        current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
-
-
-        ctx.beginPath();
-        ctx.strokeStyle = color
+function playsound(){
+	x.play();
+}
